71c40ffd0aedfcc951aa4a75785fba120d8ec09e
S0rrow <jae9hell@gmail.com>
2018-05-10 19:00:16 +0900

    prob1 comment modified

--- src/edu/handong/csee/java/Lab14/Prob1/SpeedLimiter.java
+++ src/edu/handong/csee/java/Lab14/Prob1/SpeedLimiter.java
@@ -1,24 +1,24 @@
 package edu.handong.csee.java.Lab14.Prob1;
 
-public class SpeedLimiter {
-	int limit = 0;
-	int speed = 0;
-	public SpeedLimiter(int limit, int speed) {
-		this.limit = limit;
-		this.speed = speed;
+public class SpeedLimiter {// declare class for speed limiter.
+	int limit = 0;// initiate an integer variable.
+	int speed = 0;// initiate an integer variable.
+	public SpeedLimiter(int limit, int speed) {// declare a constructor.
+		this.limit = limit;// assign parameter integer to class variable.
+		this.speed = speed;// assign parameter integer to class variable.
 	}
 	
-	public void acto() {
+	public void acto() {// declare a method to try exceptions.
 		try {
-			if(this.limit > this.speed) {
-				System.out.println("You drive like a ruler.");
+			if(this.limit > this.speed) {// set condition that limit is higher than speed.
+				System.out.println("You drive like a ruler.");// if so, print out a given string.
 			}
-			else {
-				throw new Exception("Made in Heaven.");
+			else {// if not,
+				throw new Exception("Made in Heaven.");// throw an exception.
 			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("You got a star like Grand theft auto. lol.");
+		} catch (Exception e) {// catch the exception.
+			System.out.println(e.getMessage());// print out the message for exception.
+			System.out.println("You got a star like Grand theft auto. lol.");// print out the given string.
 		}
 	}
 }
--- src/edu/handong/csee/java/Lab14/Prob1/SpeedLimiter_executor.java
+++ src/edu/handong/csee/java/Lab14/Prob1/SpeedLimiter_executor.java
@@ -1,19 +1,20 @@
 package edu.handong.csee.java.Lab14.Prob1;
-import java.util.Scanner;
-import java.util.Random;
-public class SpeedLimiter_executor {
-	public void main(String args[]) {
-		acto();
+import java.util.Scanner;// import scanner for getting input.
+import java.util.Random;// import random for generating random numbers.
+public class SpeedLimiter_executor {// declare class for executing speed limiter.
+	public static void main(String args[]) {// declare main method.
+		SpeedLimiter_executor actor = new SpeedLimiter_executor();// instantiate this class for executing internal method.
+		actor.acto();// start internal method
 	}
-	private void acto() {
-		Scanner keyboard = new Scanner(System.in);
-		Random rand = new Random();
-		System.out.println("Give me the limit:");
-		int limit = keyboard.nextInt();
-		int speed = rand.nextInt(101);
-		SpeedLimiter Speedgun = new SpeedLimiter(limit, speed);
-		Speedgun.acto();
-		keyboard.close();
+	private void acto() {// declare method that does what should be done in main.
+		Scanner keyboard = new Scanner(System.in);// declare scanner for getting inputs.
+		Random rand = new Random();// declare random for generating random numbers.
+		System.out.println("Give me the limit :");// instruct user to give limit.
+		int limit = keyboard.nextInt();// assign next given integer to variable.
+		int speed = rand.nextInt(101);// assign next given integer lower than 101 to variable.
+		SpeedLimiter Speedgun = new SpeedLimiter(limit, speed);// instantiate speed limiter.
+		Speedgun.acto();// start speed limiter.
+		keyboard.close();// close the keyboard.
 	}
 
 }
